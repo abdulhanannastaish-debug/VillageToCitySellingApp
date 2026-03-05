09c211a5403af609f3353a95b6779c526c725f52
ABDUL HANNAN <azmahafara@gmail.com>
2026-03-06 00:28:24 +0500

    committed by hannan

---
+++ app/src/main/java/com/example/villagetocityreseilingapp/buyer_fragment_orders.java
@@ -0,0 +1,21 @@
+package com.example.villagetocityreseilingapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class buyer_fragment_orders extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.buyer_fragment_orders, container, false);
+    }
+}
